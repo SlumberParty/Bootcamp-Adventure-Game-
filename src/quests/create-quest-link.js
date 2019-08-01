8e4ebe06053b55edea2c2a7b28d872f4e303afa7
Claire Cassidy <clacassidy@gmail.com>
2019-08-01 10:42:23 -0700

    first send to route

--- src/quests/create-quest-link.js
+++ src/quests/create-quest-link.js
@@ -2,6 +2,7 @@ function createQuestLink(quest) {
     const anchor = document.createElement('a');
     anchor.classList.add('quest');
     anchor.textContent = quest.title;
+    
     const searchParams = new URLSearchParams();
     searchParams.set('id', quest.id);
     anchor.href = 'quest.html?id=' + quest.id;
